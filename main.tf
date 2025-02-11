51cbbfe97aae7211d70ddda69a7872c57a1ba5ba
sbernalf <84498864+sbernalf@users.noreply.github.com>
2025-02-11 16:24:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
-    disk_size_gb         = 30
+    disk_size_gb         = 64
   }
   disable_password_authentication = false
 
