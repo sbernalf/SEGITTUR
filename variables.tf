484d33c8264cf753fc51c842aac5a4050c067f92
sbernalf <84498864+sbernalf@users.noreply.github.com>
2025-02-11 16:59:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -38,14 +38,14 @@ variable "script_urls" {
   description = "Lista de URLs de los scripts de configuración"
   type        = list(string)
   default     = [
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_03-Parametros_del_kernel.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_04-Parametros_SSH.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_05-Manipulacion_de_registros_de_actividad.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_06-Desinstalar_usuarios_innecesarios.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_07-intentos_fallidos.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_09-Parametros_gnome.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_10-Elementos_innecesarios.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_11-Paquetes_huerfanos.sh",
-    "https://raw.github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_Limitacion_usb.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_03-Parametros_del_kernel.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_04-Parametros_SSH.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_05-Manipulacion_de_registros_de_actividad.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_06-Desinstalar_usuarios_innecesarios.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_07-intentos_fallidos.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_09-Parametros_gnome.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_10-Elementos_innecesarios.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_11-Paquetes_huerfanos.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_Limitacion_usb.sh",
   ]
 }
