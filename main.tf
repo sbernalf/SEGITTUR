b1d04e172004f81beb6833969be923a3fcb6a323
sbernalf <84498864+sbernalf@users.noreply.github.com>
2025-02-11 13:02:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+    #public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
 }
 
