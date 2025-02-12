22fbe6a7e86df2a910e321755d99c7f3b687a19a
sbernalf <84498864+sbernalf@users.noreply.github.com>
2025-02-12 18:51:27 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -40,6 +40,6 @@ variable "script_urls" {
   default     = [
     "https://stgsegittur.blob.core.windows.net/scriptsens/Post-Config_Script.sh",
     "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_03-Parametros_del_kernel.sh",
-    "https://github.com/sbernalf/SEGITTUR/blob/main/CCN-STIC-610A22-Script/CCN-STIC-610A22_05-Manipulacion_de_registros_de_actividad.sh",
+    "https://stgsegittur.blob.core.windows.net/scriptsens/CCN-STIC-610A22_05-Manipulacion_de_registros_de_actividad.sh",
   ]
 }
