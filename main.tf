49d836df67bd69b9fe7298934284c7ddc0f2179d
sbernalf <84498864+sbernalf@users.noreply.github.com>
2025-02-13 17:04:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,14 +36,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "publicip" {
-  name                = "publicip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                  = "Standard"
-}
-
 resource "azurerm_subnet" "subnetBastion" {
   name                 = "AzureBastionSubnet"  # Nombre correcto de la subred
   resource_group_name  = azurerm_resource_group.rg.name
